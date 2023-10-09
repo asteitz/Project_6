0cb664cfd36cc40e2958db0e6a0da457ad632d29
asteitz <asteitz@iu.edu>
2023-10-09 19:40:17 -0400

    Gradel and Note

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+//    id("com.google.devtools.ksp")
+//    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -33,22 +35,31 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures{
-        viewBinding = true
-        dataBinding =  true
+//
+//    kotlinOptions {
+//        jvmTarget = JavaVersion.VERSION_17.toString()
+//    }
+    buildFeatures {
+        dataBinding = true
     }
 }
 
 dependencies {
-
-    implementation ("com.android.support:appcompat-v7:33.0.0")
-    implementation ("com.android.support:recyclerview-v7:33.0.0")
+    val lifecycle_version = "2.3.1"
+    val room_version = "2.4.1"
+    annotationProcessor("androidx.room:room-compiler:$room_version")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+    implementation ("androidx.room:room-runtime:$room_version")
+    implementation ("androidx.room:room-ktx:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/project6/NoteDao.kt
@@ -0,0 +1,21 @@
+package com.example.project6
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NoteDao {
+    @Insert
+    suspend fun insert(task: Note)
+    @Update
+    suspend fun update(task: Note)
+    @Delete
+    suspend fun delete(task: Note)
+    @Query("SELECT * FROM task_table WHERE taskId = :key")
+    fun get(key: Long): LiveData<Note>
+    @Query("SELECT * FROM task_table ORDER BY taskId DESC")
+    fun getAll(): LiveData<List<Note>>
+}
